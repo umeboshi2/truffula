f0d0304fdbf1d9104e2d42484bee15954a3b33fe
Joseph Rawson <joseph.rawson.works@littledebian.org>
2014-10-11 12:04:46 -0500

    added basic views

--- truffula/__init__.py
+++ truffula/__init__.py
@@ -1,15 +1,32 @@
 from pyramid.config import Configurator
 from sqlalchemy import engine_from_config
 
+from trumpet.models.usergroup import User, Password
+
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     engine = engine_from_config(settings, 'sqlalchemy.')
+    settings['db.sessionmaker'] = DBSession
+    settings['db.usermodel'] = User
+    settings['db.password_model'] = Password
+    settings['db.usernamefield'] = 'username'
+    
+
+
     DBSession.configure(bind=engine)
     Base.metadata.bind = engine
+
+
+    root_factory = 'trumpet.resources.RootGroupFactory'
+    request_factory = 'trumpet.request.AlchemyRequest'
+    
     config = Configurator(settings=settings)
-    config.include('pyramid_chameleon')
+    includes = ['cornice', 'pyramid_beaker']
+    for i in includes:
+        config.include(i)
+    client_view = 'truffula.views.client.ClientView'
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.scan()
